9762565a273f41190b85715ce03e523e69696b0e
david <david@debian-BULLSEYE-live-builder-AMD64>
2023-06-23 22:03:12 -0500

    r

--- preguntas.py
+++ preguntas.py
@@ -47,22 +47,22 @@ def pregunta_02():
     """
 
     # Lea el archivo `gm_2008_region.csv` y asignelo al DataFrame `df`
-    df = ____
+    df = pd.read_csv('gm_2008_region.csv')
 
     # Imprima las dimensiones del DataFrame
-    print(____.____)
+    print(df.shape)
 
     # Imprima la correlación entre las columnas `life` y `fertility` con 4 decimales.
-    print(____)
+    print(df['life'].corr(df['fertility']).round(4))
 
     # Imprima la media de la columna `life` con 4 decimales.
-    print(____)
+    print(df['life'].mean().round(4))
 
     # Imprima el tipo de dato de la columna `fertility`.
-    print(____)
+    print(df['fertility'].dtype)
 
     # Imprima la correlación entre las columnas `GDP` y `life` con 4 decimales.
-    print(____)
+    print(df['GDP'].corr(df['life']).round(4))
 
 
 def pregunta_03():
